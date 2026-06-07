e80e77caa347b6115ccb9ddfedb48a28c709e9be
masungil70 <103398786+masungil70@users.noreply.github.com>
2026-06-07 14:51:00 +0900

    Update step24.py

--- day12/langchain-chatbot/step24.py
+++ day12/langchain-chatbot/step24.py
@@ -1,16 +1,17 @@
 from langchain_core.runnables import (
+    RunnableParallel,
     RunnablePassthrough,
     RunnableLambda
 )
 
 # LCEL Runnable 체인 구성
-chain = {
+chain = RunnableParallel({
     "context": RunnableLambda(
         lambda x: f"검색 결과: {x}"
     ),
 
     "question": RunnablePassthrough()
-}
+})
 
 # LCEL Runnable 체인 실행
 print(
