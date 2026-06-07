8e0c5f1dd919e407dfa6153bfa544be752e7d882
masungil70 <103398786+masungil70@users.noreply.github.com>
2026-06-07 14:51:27 +0900

    Update step25.py

--- day12/langchain-chatbot/step25.py
+++ day12/langchain-chatbot/step25.py
@@ -1,16 +1,17 @@
 from langchain_core.runnables import (
+    RunnableParallel,
     RunnablePassthrough,
     RunnableLambda
 )
 
 # LCEL Runnable 체인 구성
-chain = {
+chain = RunnableParallel({
     "name": RunnablePassthrough(),
 
     "message": RunnableLambda(
         lambda x: f"{x}님 반갑습니다."
     )
-}
+})
 
 # LCEL Runnable 체인 실행
 result = chain.invoke(
